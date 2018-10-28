e42c02e68bf3880ce149f96e0e83cfeba4ad7a5c
lukaszvikas <lzvikas1@gmail.com>
2018-10-27 23:15:30 -0400

    Task: created a database seed to generate 50 cars

--- database/factories/CarsFactory.php
+++ database/factories/CarsFactory.php
@@ -6,7 +6,7 @@ use Faker\Generator as Faker;
     $factory->define(App\Car::class, function (Faker $faker) {
         return [
             'make' => $faker->randomElement($array = array('ford', 'honda', 'toyota')),
-            'model' => $faker->name,
+            'model' => $faker->randomNumber($nbDigits = null, $strict = false),
             'year' => $faker->year($max = '2018')
 
         ];
