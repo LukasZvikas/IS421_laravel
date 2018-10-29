dfb1923ce651d921abccb84c2e364382db0913c7
lukaszvikas <lzvikas1@gmail.com>
2018-10-29 00:07:52 -0400

    Task: wrote test to check if register returns 200 status code

---
+++ tests/Feature/AuthFeatureTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lukas
+ * Date: 10/29/18
+ * Time: 12:00 AM
+ */
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+
+
+class AuthTest extends TestCase {
+
+    function testRegisterPage() {
+
+        $response = $this->get("/register");
+
+        $response->assertStatus(200);
+    }
+
+
+}
+
+
+
+?>
